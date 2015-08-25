3942ab889f4c89ac160c1580ea86e6c6b436bbae
hokerlong <hoker.long@gmail.com>
2015-08-25 14:10:23 -0700

    Update setup_lamp.sh

--- sysadm/setup_lamp.sh
+++ sysadm/setup_lamp.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# LAMP
 sudo yum install -y httpd24 php56 mysql55-server php56-mysqlnd php56-mbstring git
+
+# phpMyAdmin
+sudo yum install --enablerepo=epel -y phpmyadmin php-phpseclib*
