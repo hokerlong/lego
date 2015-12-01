6b03efd2575d001b83c1a1c3f24a257ec71883ab
hokerlong <username@users.noreply.github.com>
2015-11-30 23:25:15 -0800

    fix

--- sysadm/taobao_price.sh
+++ sysadm/taobao_price.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ps u | grep taobao | awk '{print $2}' | xargs -I{} kill {}
+ps ux | grep taobao | awk '{print $2}' | xargs -I{} kill {}
 cd ~/htdoc;
 php taobao_price.php >> ~/logs/taobao_price.log
