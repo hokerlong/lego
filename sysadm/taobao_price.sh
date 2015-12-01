e6f15c11f57e22f6e814dd553481c22758d03eb7
hokerlong <username@users.noreply.github.com>
2015-12-01 00:11:27 -0800

    fix

--- sysadm/taobao_price.sh
+++ sysadm/taobao_price.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-ps ux | grep -v grep | grep taobao | awk '{print $2}' | xargs -I{} kill {};
+PID=`ps ux | grep taobao_price | grep -v grep | awk '{print $2}'`
+if [[ "" !=  "$PID" ]]; then
+  echo "killing $PID"
+  kill -9 $PID
+fi
+
 /usr/bin/php /home/ec2-user/htdoc/taobao_price.php >> /home/ec2-user//logs/taobao_price.log 2>&1;
 
